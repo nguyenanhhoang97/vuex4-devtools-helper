87a822255bd4bb9d8a92cd8d9ddbae73f9dfc8d9
Anh Hoang Nguyen <nguyenanhhoang.ict@gmail.com>
2021-05-13 16:14:53 +0700

    v1

---
+++ constants.js
@@ -0,0 +1,12 @@
+export const COLOR_LIME_500 = 0x84cc16;
+export const COLOR_DARK = 0x666666;
+export const COLOR_WHITE = 0xffffff;
+export const TAG_NAMESPACED = {
+  label: "namespaced",
+  textColor: COLOR_WHITE,
+  backgroundColor: COLOR_DARK,
+};
+export const LABEL_VUEX_BINDINGS = "vuex bindings";
+export const MUTATIONS_LAYER_ID = "vuex:mutations";
+export const ACTIONS_LAYER_ID = "vuex:actions";
+export const INSPECTOR_ID = "vuex";
